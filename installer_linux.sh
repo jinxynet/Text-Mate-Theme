6818c84240e4e429181e4ba8d88b62451bc79442
Vivek Gohel <122967043+vivekgohel2004@users.noreply.github.com>
2025-08-22 11:58:23 +0530

    Update installer_linux.sh

--- installer_linux.sh
+++ installer_linux.sh
@@ -2,13 +2,25 @@
 
 GREEN="\e[32m"
 INSTALL_PATH="/home/$USER/.config/sublime-text/Packages/User/"
+THEME_URL="https://raw.githubusercontent.com/vivekgohel2004/Text-Mate-Theme/main/Text-Mate.sublime-color-scheme"
+THEME_FILE="Text-Mate.sublime-color-scheme"
 
-echo -e "${GREEN}[+] Installing..."
+echo -e "${GREEN}[+] Installing Text-Mate Theme..."
 
-if ! test -d $INSTALL_PATH; then 
-    mkdir -p $INSTALL_PATH 
+# Create install directory if it doesn't exist
+if ! test -d "$INSTALL_PATH"; then 
+    mkdir -p "$INSTALL_PATH"
 fi
 
-cp Text-Mate.sublime-color-scheme $INSTALL_PATH
+# Download theme file if not present
+if ! test -f "$THEME_FILE"; then
+    echo -e "${GREEN}[+] Downloading $THEME_FILE..."
+    curl -s -O "$THEME_URL"
+fi
 
-echo -e "${GREEN} [+] File installed at $INSTALL_PATH. Please restart sublime text if open"
+# Copy theme file to Sublime User folder
+cp "$THEME_FILE" "$INSTALL_PATH"
+
+echo -e "${GREEN}[+] File installed at $INSTALL_PATH"
+echo -e "${GREEN}[+] Please restart Sublime Text if it is open."
+
